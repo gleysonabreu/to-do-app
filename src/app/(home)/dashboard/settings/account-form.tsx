fa3d572e09eda0af9312e2aee01084e3fa3a8f8b
Gleyson Abreu <gleyson_datu@hotmail.com>
2023-10-20 12:08:01 -0300

    add first look dashboard settings

---
+++ src/app/(home)/dashboard/settings/account-form.tsx
@@ -0,0 +1,106 @@
+'use client';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
+
+import { Button } from '@/components/ui/button';
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { toast } from '@/components/ui/use-toast';
+
+const accountFormSchema = z.object({
+  firstName: z
+    .string()
+    .min(2, {
+      message: 'First name must be at least 2 characters.',
+    })
+    .max(30, {
+      message: 'First name must not be longer than 30 characters.',
+    }),
+  lastName: z
+    .string()
+    .min(2, {
+      message: 'First name must be at least 2 characters.',
+    })
+    .max(30, {
+      message: 'First name must not be longer than 30 characters.',
+    }),
+});
+
+type AccountFormValues = z.infer<typeof accountFormSchema>;
+
+const defaultValues: Partial<AccountFormValues> = {};
+
+export function AccountForm() {
+  const form = useForm<AccountFormValues>({
+    resolver: zodResolver(accountFormSchema),
+    defaultValues,
+  });
+
+  function onSubmit(data: AccountFormValues) {
+    toast({
+      title: 'You submitted the following values:',
+      description: (
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      ),
+    });
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="firstName"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>First name</FormLabel>
+              <FormControl>
+                <Input placeholder="First name" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is the first name that will be displayed on your profile
+                and in emails.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="lastName"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Last name</FormLabel>
+              <FormControl>
+                <Input placeholder="Last name" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is the last name that will be displayed on your profile and
+                in emails.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button type="submit" variant="sky">
+          Update account
+        </Button>
+      </form>
+    </Form>
+  );
+}
---
+++ src/app/(home)/dashboard/settings/profile/page.tsx
@@ -0,0 +1,3 @@
+export default function Settings() {
+  return <main className="flex-1">hello profile</main>;
+}
