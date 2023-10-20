c5ebebaf0d8d1306e958353dcb427c2f4778453d
Gleyson Abreu <gleyson_datu@hotmail.com>
2023-10-20 14:02:32 -0300

    dashboard: add message if no have to-do

--- src/app/(home)/dashboard/page.tsx
+++ src/app/(home)/dashboard/page.tsx
@@ -1,6 +1,7 @@
 import { TodoItem } from '@/components/to-do-item';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { CircleOff } from 'lucide-react';
 
 export type Todo = {
   id: string;
@@ -27,19 +28,31 @@ export default async function Dashboard() {
   const todos = responseBody.todos as Todo[];
 
   return (
-    <main className="flex-1 p-6 w-full">
-      <div className="grid justify-items-center md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-        {todos.reverse().map((todo) => (
-          <TodoItem
-            key={todo.id}
-            id={todo.id}
-            title={todo.title}
-            description={todo.description}
-            amount={todo.amount}
-            completed={todo.completed}
-          />
-        ))}
-      </div>
+    <main className="flex flex-col flex-1 p-6 w-full">
+      {todos.length === 0 ? (
+        <div className="flex flex-1 items-center justify-center">
+          <div className="flex items-center flex-col gap-2 max-w-md w-full p-10 border border-dashed rounded-lg border-border">
+            <CircleOff size={30} />
+            <h1 className="font-bold text-lg">No to-dos</h1>
+            <span className="text-muted-foreground text-sm">
+              Get started by creating a new to-do.
+            </span>
+          </div>
+        </div>
+      ) : (
+        <div className="grid justify-items-center md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+          {todos.reverse().map((todo) => (
+            <TodoItem
+              key={todo.id}
+              id={todo.id}
+              title={todo.title}
+              description={todo.description}
+              amount={todo.amount}
+              completed={todo.completed}
+            />
+          ))}
+        </div>
+      )}
     </main>
   );
 }
