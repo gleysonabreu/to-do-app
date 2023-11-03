cf1a5ee8caffe2b559a670ba6a7d032935c82128
Gleyson Abreu <gleyson_datu@hotmail.com>
2023-11-03 17:01:22 -0300

    feat: add profile page and show tasks

---
+++ src/app/(home)/profile/[username]/page.tsx
@@ -0,0 +1,83 @@
+import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar';
+import { Metadata } from 'next';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+
+import { User } from '@/types/user';
+import { api } from '@/config/api';
+import { notFound } from 'next/navigation';
+import { TabTasks } from './tab-tasks';
+
+export async function generateMetadata({
+  params,
+}: ProfileProps): Promise<Metadata> {
+  const username = params.username;
+
+  const user = await getUserByUsername(username);
+
+  return {
+    title: `Profile - ${user?.firstName} ${user?.lastName}(@${user?.username})`,
+  };
+}
+
+type ProfileProps = {
+  params: {
+    username: string;
+  };
+};
+
+async function getUserByUsername(username: string): Promise<User | undefined> {
+  const response = await api(`/users/${username}`);
+
+  if (!response.ok) {
+    return undefined;
+  }
+
+  const data = await response.json();
+
+  return {
+    ...data.user,
+    firstName: data.user.first_name,
+    lastName: data.user.last_name,
+  };
+}
+
+export default async function Profile({ params }: ProfileProps) {
+  const user = await getUserByUsername(params.username);
+
+  if (!user || !user.isPublic) {
+    notFound();
+  }
+
+  return (
+    <div className="flex-1 mx-8 lg:mx-20 mt-10">
+      <header className="flex gap-3">
+        <Avatar className="h-14 w-14">
+          <AvatarImage src={user.username} />
+          <AvatarFallback className="uppercase">
+            {user.firstName.slice(0, 2)}
+          </AvatarFallback>
+        </Avatar>
+
+        <div>
+          <h1 className="text-md font-bold">
+            {user.firstName} {user.lastName}
+          </h1>
+          <span className="text-sm text-muted-foreground">@gleysonabreu</span>
+        </div>
+      </header>
+
+      <div className="flex-col md:flex">
+        <div className="flex-1 space-y-4 p-8 pt-6 pl-0">
+          <Tabs defaultValue="tasks" className="space-y-4">
+            <TabsList>
+              <TabsTrigger value="tasks">Tasks</TabsTrigger>
+            </TabsList>
+            <TabsContent value="tasks" className="space-y-4">
+              <TabTasks username={user.username} userId={user.id} />
+            </TabsContent>
+          </Tabs>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/(home)/profile/[username]/tab-tasks.tsx
@@ -0,0 +1,34 @@
+import { TodoItem } from '@/components/to-do-item';
+import { api } from '@/config/api';
+import { Todo } from '@/types/todo';
+import { NoTodos } from '../../dashboard/todos/components/no-to-dos';
+
+type TabTasksProps = {
+  userId: string;
+  username: string;
+};
+
+async function getTodos(userId: string): Promise<Todo[]> {
+  const response = await api(`/users/${userId}/todos`);
+
+  const data = await response.json();
+  return data.todos;
+}
+
+export async function TabTasks({ userId, username }: TabTasksProps) {
+  const todos = await getTodos(userId);
+
+  return (
+    <div>
+      {todos.length === 0 ? (
+        <NoTodos />
+      ) : (
+        <div className="grid justify-items-center md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+          {todos.map((todo) => (
+            <TodoItem key={todo.id} {...todo} url={`${username}/todo/any-id`} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
